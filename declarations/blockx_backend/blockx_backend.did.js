4af706cd3f1b88650b70ac7f31d9542f61e38ff9
Shaker Sharabi <shakersharabi100@gmail.com>
2025-08-19 14:38:02 +0300

    Update declarations

--- declarations/blockx_backend/blockx_backend.did.js
+++ declarations/blockx_backend/blockx_backend.did.js
@@ -1,9 +1,4 @@
 export const idlFactory = ({ IDL }) => {
-  const User = IDL.Record({
-    'username' : IDL.Text,
-    'password' : IDL.Text,
-    'email' : IDL.Text,
-  });
   const AssetType = IDL.Variant({
     'Physical' : IDL.Null,
     'NonPhysical' : IDL.Null,
@@ -32,16 +27,42 @@ export const idlFactory = ({ IDL }) => {
     'serial_number' : IDL.Opt(IDL.Text),
     'registeration_number' : IDL.Opt(IDL.Text),
   });
+  const Asset = IDL.Record({
+    'asset_type' : AssetType,
+    'owner' : IDL.Principal,
+    'hash' : IDL.Text,
+    'created_at' : IDL.Nat,
+    'details' : AssetDetails,
+    'category' : AssetCategory,
+    'ownership_proof' : Proof,
+  });
+  const User = IDL.Record({
+    'username' : IDL.Text,
+    'password' : IDL.Text,
+    'email' : IDL.Text,
+  });
   const AssetUserInput = IDL.Record({
     'asset_type' : AssetType,
     'details' : AssetDetails,
     'category' : AssetCategory,
     'ownership_proof' : Proof,
   });
+  const ServiceError = IDL.Variant({
+    'InvalidCategory' : IDL.Text,
+    'NotFound' : IDL.Text,
+    'AssetAlreadyExists' : IDL.Text,
+    'Unauthorized' : IDL.Text,
+    'MissingProof' : IDL.Text,
+    'InternalError' : IDL.Text,
+  });
+  const Result = IDL.Variant({ 'Ok' : IDL.Text, 'Err' : ServiceError });
   return IDL.Service({
     'add_user' : IDL.Func([IDL.Text, IDL.Text, IDL.Text], [IDL.Nat], []),
+    'get_asset' : IDL.Func([IDL.Nat], [Asset], ['query']),
+    'get_ic_timestamp' : IDL.Func([], [IDL.Nat], ['query']),
+    'get_system_timestamp' : IDL.Func([], [IDL.Nat], ['query']),
     'get_user' : IDL.Func([IDL.Nat], [User], ['query']),
-    'register_asset' : IDL.Func([AssetUserInput], [IDL.Text], []),
+    'register_asset' : IDL.Func([AssetUserInput], [Result], []),
   });
 };
 export const init = ({ IDL }) => { return []; };
