9f50cedd229b7da5af7be983505ef9851693458e
Shaker Sharabi <shakersharabi100@gmail.com>
2025-08-20 05:45:44 +0300

    Add dynamic time

--- backend/src/utils.rs
+++ backend/src/utils.rs
@@ -1,28 +1,23 @@
 use std::time::{SystemTime, UNIX_EPOCH};
-use ic_cdk::api::time;
 use candid::Principal;
 use ic_cdk::api::msg_caller;
 
-#[ic_cdk_macros::query]
-// gets the timestamp out of the canister
-pub fn get_system_timestamp() -> u128 {
-    let start = SystemTime::now();
-    let since_the_epoch = start
-        .duration_since(UNIX_EPOCH)
-        .expect("Failed to get time since UNIX_EPOCH");
-    let milliseconds = since_the_epoch.as_millis();
-    milliseconds
-}
-
-#[ic_cdk_macros::query]
-// gets the timestamp after deploying the canister
-pub fn get_ic_timestamp() -> u128 {
-    time().into()
-}
-
-// one interface for easily changing
+// gets the timestamp
 pub fn get_timestamp() -> u128 {
-    get_ic_timestamp()
+    #[cfg(target_arch = "wasm32")]
+    {
+        ic_cdk::api::time().into()
+    }
+
+    #[cfg(not(target_arch = "wasm32"))]
+    {
+        let start = SystemTime::now();
+        let since_the_epoch = start
+            .duration_since(UNIX_EPOCH)
+            .expect("Failed to get time since UNIX_EPOCH");
+        let milliseconds = since_the_epoch.as_millis();
+        milliseconds
+    }
 }
 
 pub fn is_authenticated() -> bool {
