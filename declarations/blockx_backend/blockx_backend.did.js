bc8e4203b59a22991e1a39420bf5ca198d631149
Shaker Sharabi <shakersharabi100@gmail.com>
2025-08-26 22:54:21 +0300

    Update declarations

--- declarations/blockx_backend/blockx_backend.did.js
+++ declarations/blockx_backend/blockx_backend.did.js
@@ -4,7 +4,7 @@ export const idlFactory = ({ IDL }) => {
     'NonPhysical' : IDL.Null,
   });
   const AssetDetails = IDL.Record({
-    'files' : IDL.Vec(IDL.Vec(IDL.Nat8)),
+    'files' : IDL.Vec(IDL.Text),
     'manufacturer' : IDL.Opt(IDL.Text),
     'name' : IDL.Text,
     'type' : IDL.Opt(IDL.Text),
@@ -20,12 +20,12 @@ export const idlFactory = ({ IDL }) => {
     'Equipment' : IDL.Null,
   });
   const Proof = IDL.Record({
-    'deed_document' : IDL.Opt(IDL.Vec(IDL.Nat8)),
+    'deed_document' : IDL.Vec(IDL.Text),
     'deed_reference_number' : IDL.Opt(IDL.Text),
-    'publication_links' : IDL.Opt(IDL.Text),
+    'publication_links' : IDL.Vec(IDL.Text),
+    'registration_number' : IDL.Opt(IDL.Text),
     'license_plate' : IDL.Opt(IDL.Text),
     'serial_number' : IDL.Opt(IDL.Text),
-    'registeration_number' : IDL.Opt(IDL.Text),
   });
   const Asset = IDL.Record({
     'asset_type' : AssetType,
@@ -58,11 +58,11 @@ export const idlFactory = ({ IDL }) => {
   const Result = IDL.Variant({ 'Ok' : IDL.Text, 'Err' : ServiceError });
   return IDL.Service({
     'add_user' : IDL.Func([IDL.Text, IDL.Text, IDL.Text], [IDL.Nat], []),
-    'get_asset' : IDL.Func([IDL.Nat], [Asset], ['query']),
-    'get_ic_timestamp' : IDL.Func([], [IDL.Nat], ['query']),
-    'get_system_timestamp' : IDL.Func([], [IDL.Nat], ['query']),
+    'get_assets' : IDL.Func([IDL.Vec(IDL.Nat)], [IDL.Vec(Asset)], ['query']),
     'get_user' : IDL.Func([IDL.Nat], [User], ['query']),
+    'get_user_assets' : IDL.Func([], [IDL.Vec(Asset)], ['query']),
     'register_asset' : IDL.Func([AssetUserInput], [Result], []),
+    'verify_asset' : IDL.Func([IDL.Text, AssetCategory], [IDL.Bool], ['query']),
   });
 };
 export const init = ({ IDL }) => { return []; };
